17ea708cc0992cb562b52a00c06f379943a15baa
Roberto Piancatelli <piancatellirob@gmail.com>
2021-03-20 11:29:34 +0000

    added validations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,12 @@
 class Venue < ApplicationRecord
+  # REFERENCES
   has_many :bids, through: :drinks_opportunity
   belongs_to :user
   has_many :drinks_opportunities
+  # VALIDATIONS
+  validations :venue_name, presence: true
+  validations :description, presence: true
+  validations :region, presence: true
+  validations :city, presence: true
+  validations :post_code, presence: true
 end
