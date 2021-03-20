8f16a63c461d49054aa0fc51c995edd7dbea523c
Roberto Piancatelli <piancatellirob@gmail.com>
2021-03-20 10:07:35 +0000

    added user to venue

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_095802) do
+ActiveRecord::Schema.define(version: 2021_03_20_100156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bids", force: :cascade do |t|
+    t.boolean "is_accepted", default: false
+    t.string "contract"
+    t.integer "price"
+    t.text "notes"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
 
   create_table "drinks", force: :cascade do |t|
     t.string "type"
@@ -40,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_03_20_095802) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bids", "users"
 end
